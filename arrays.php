377d0630340b12b670ab16eef8344e8f394c7b8a
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-10-15 19:13:20 +0530

    arrays file added

---
+++ arrays.php
@@ -0,0 +1,30 @@
+<pre>
+<?php
+    $arr=['tokyo','dubai','hongkong','medilline','warsaw'];
+
+    $subjects= ['HTML','CSS','BOOTSTRAP','JAVASCRIPT','AJAX','JSON','JQUERY','REACTJS','PHP&MYSQL','LARAVEL','DOCKER','AWS'];
+    
+    $mix =["hello",10,69.69,true,[10,20,30,40,50]];
+
+    $rakshak =[
+        'name'=>'Shay ',
+        'country'=>'Japan',
+        'city'=>'Yokohama',
+        'networth'=>'$800000000000.00'
+    ];
+
+   
+
+    print_r($subjects);
+    print_r($rakshak);
+    
+    print $rakshak['name'];
+
+    print_r($mix);
+
+    echo $mix[4][3];
+
+    $arr1=[112,34,654,1=>23,45,87,23,654,3423];
+    print_r($arr1);
+?>
+</pre>
